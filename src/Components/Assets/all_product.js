c36052153e3e0e37d923c01726ae2a53067cca49
espurrell <150044207+espurrell@users.noreply.github.com>
2024-04-08 11:52:28 -0230

    copy of final sprint for the project

---
+++ src/Components/Assets/all_product.js
@@ -0,0 +1,337 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
+import p7_img from "./product_7.png";
+import p8_img from "./product_8.png";
+import p9_img from "./product_9.png";
+import p10_img from "./product_10.png";
+import p11_img from "./product_11.png";
+import p12_img from "./product_12.png";
+import p13_img from "./product_13.png";
+import p14_img from "./product_14.png";
+import p15_img from "./product_15.png";
+import p16_img from "./product_16.png";
+import p17_img from "./product_17.png";
+import p18_img from "./product_18.png";
+import p19_img from "./product_19.png";
+import p20_img from "./product_20.png";
+import p21_img from "./product_21.png";
+import p22_img from "./product_22.png";
+import p23_img from "./product_23.png";
+import p24_img from "./product_24.png";
+import p25_img from "./product_25.png";
+import p26_img from "./product_26.png";
+import p27_img from "./product_27.png";
+import p28_img from "./product_28.png";
+import p29_img from "./product_29.png";
+import p30_img from "./product_30.png";
+import p31_img from "./product_31.png";
+import p32_img from "./product_32.png";
+import p33_img from "./product_33.png";
+import p34_img from "./product_34.png";
+import p35_img from "./product_35.png";
+import p36_img from "./product_36.png";
+
+let all_product = [
+  {
+    id: 1,
+    name: "Red Suede Chair",
+    category: "living",
+    description_long: "Red Suede Chair with solid wood legs and a high density foam cushion. This chair is perfect for any living room or office space.",
+    image: p1_img,
+    new_price: 63.75,
+    old_price: 75.00,
+    
+  },
+  {
+    id: 2,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "seating",
+    image: p2_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 3,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "seating",
+    image: p3_img,
+    new_price: 60.00,
+    old_price: 100.50,
+  },
+  {
+    id: 4,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "seating",
+    image: p4_img,
+    new_price: 100.00,
+    old_price: 150.00,
+  },
+  {
+    id: 5,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "women",
+    image: p5_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 6,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "women",
+    image: p6_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 7,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "women",
+    image: p7_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 8,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "women",
+    image: p8_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 9,
+    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    category: "women",
+    image: p9_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 10,
+    name: "Wall Organization Unit",
+    category: "office",
+    description_long: "A versatile organization unit that works in many spaces. Stylish, modern, will keep your space organzied and looking great.",
+    image: p10_img,
+    new_price: 424.99,
+    old_price: 499.99,
+  },
+  {
+    id: 11,
+    name: "Cube Bookshelf",
+    category: "living",
+    description_long: "A cube bookshelf with 12 compartments for easy organization. Perfect for any office space.",
+    image: p11_img,
+    new_price: 110.49,
+    old_price: 129.99,
+  },
+  {
+    id: 12,
+    name: "White Trestle Desk",
+    category: "office",
+    description_long: "A compact trestle desk, perfect for a smaller space. Home office, student study space, this desk is the perfect addition for any room.",
+    image: p12_img,
+    new_price: 297.49,
+    old_price: 349.99,
+  },
+  {
+    id: 13,
+    name: "White Trestle Desk",
+    category: "office",
+    description_long: "White Trestle Desk with built in organization and cable management.One leg has 5 storage compartments for easy organization. This desk is perfect for any office space. ",
+    image: p13_img,
+    new_price: 509.99,
+    old_price: 599.99,
+  },
+  {
+    id: 14,
+    name: "Men Tri-Colored Solid ",
+    category: "men",
+    description_short:"Men Tri-Colored Solid Zippered Full-Zip Slim Fit Bomber Jacket" ,
+    description_long: "Men Tri-Colored Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p14_img,
+    new_price: 85.99,
+    old_price: 120.99,
+  },
+  {
+    id: 15,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p15_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 16,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p16_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 17,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p17_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 18,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p18_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 19,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p19_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 20,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p20_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 21,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p21_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 22,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p22_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 23,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p23_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 24,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "men",
+    image: p24_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 25,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p25_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 26,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p26_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 27,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p27_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 28,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p28_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 29,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p29_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 30,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p30_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 31,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p31_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 32,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p32_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 33,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p33_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 34,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p34_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 35,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p35_img,
+    new_price: 85.00,
+    old_price: 120.50,
+  },
+  {
+    id: 36,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kid",
+    image: p36_img,
+    new_price: 85.00,
+    old_price: 120.59,
+  },
+];
+
+export default all_product;
